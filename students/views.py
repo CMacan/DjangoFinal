f005e3c6ba67fb5c4e9b1108d5cfd2eff5951e36
Charles <macancharles9@gmail.com>
2024-06-02 11:00:30 +0800

    footer fixed

--- students/views.py
+++ students/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from .models import Student
 
 
@@ -7,3 +9,7 @@ def index(request):
     return render(request, 'students/index.html', {
         'students': Student.objects.all()
     })
+
+def view_student(request, id):
+    student = Student.objects.get(pk = id)
+    return HttpResponseRedirect(reverse('index'))
