625f528a184fe33c42ba4fce0621e8cf7031d634
Charles <macancharles9@gmail.com>
2024-06-02 13:35:09 +0800

    finiiished

--- students/forms.py
+++ students/forms.py
@@ -4,10 +4,10 @@ from .models import Student
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ['student_number', 'last_name', 'email', 'field_of_study']
+        fields = ['student_number', 'first_name', 'last_name', 'email', 'field_of_study']
         labels = {
             'student_number': 'Student Number',
-            'fist_name': 'First Name',
+            'first_name': 'First Name',
             'last_name': 'Last Name',
             'email': 'Email',
             'field_of_study': 'Field of Study'
@@ -15,7 +15,7 @@ class StudentForm(forms.ModelForm):
         widgets = {
             'student_number': forms.NumberInput(attrs={'class':'form-control'}),
             'student_number': forms.TextInput(attrs={'class':'form-control'}),
-            'fist_name': forms.TextInput(attrs={'class':'form-control'}),
+            'first_name': forms.TextInput(attrs={'class':'form-control'}),
             'last_name': forms.TextInput(attrs={'class':'form-control'}),
             'email': forms.EmailInput(attrs={'class':'form-control'}),  
             'field_of_study': forms.TextInput(attrs={'class':'form-control'}),
